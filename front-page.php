2348e4a627048cb43c318660192dc319bb547a46
yumauda <firststep-yumauda@gmail.com>
2024-12-19 11:36:37 +0900

    topページセクション3つ分

--- front-page.php
+++ front-page.php
@@ -5,39 +5,110 @@
       <div class="p-top-choose__content">
         <div class="p-top-choose__title">
           <h3 class="c-main-title">
-            小学校から高校まで一貫して通える
+            小学校から高校まで<br class="u-mobile">一貫して通える
           </h3>
         </div>
         <ul class="p-top-choose__lists">
           <li class="p-top-choose__list">
+
             <figure class="p-top-choose__img">
-              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/top_recruit_1.webp" alt="小学生" width="350" height="350">
+              <picture>
+                <source srcset="<?php echo get_template_directory_uri() ?>/images/common/top_recruit_1.webp" media="(min-width: 768px)" width="350" height="350" />
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/top_recruit_1_sp.jpg" alt="小学生" width="350" height="190">
+              </picture>
             </figure>
             <div class="p-top-choose__detail">
               <h4 class="p-top-choose__list-title">小学生</h4>
               <p class="p-top-choose__text">メリハリのある授業で楽しく成績アップ！</p>
-              <a href="#" class="p-top-choose__btn">詳しくはこちら</a>
+              <div class="p-top-choose__btn-wrapper">
+                <a href="#" class="p-top-choose__btn">詳しくはこちら</a>
+              </div>
             </div>
           </li>
           <li class="p-top-choose__list">
             <figure class="p-top-choose__img">
-              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/top_recruit_2.webp" alt="中学生" width="350" height="350">
+              <picture>
+                <source srcset="<?php echo get_template_directory_uri() ?>/images/common/top_recruit_2.webp" media="(min-width: 768px)" width="350" height="350" />
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/top_recruit_2_sp.jpg" alt="中学生" width="350" height="190">
+              </picture>
             </figure>
             <div class="p-top-choose__detail">
               <h4 class="p-top-choose__list-title">中学生</h4>
               <p class="p-top-choose__text">県内の中学校データや判断で適切な指導を</p>
-              <a href="#" class="p-top-choose__btn">詳しくはこちら</a>
+              <div class="p-top-choose__btn-wrapper">
+                <a href="#" class="p-top-choose__btn">詳しくはこちら</a>
+              </div>
             </div>
           </li>
           <li class="p-top-choose__list">
             <figure class="p-top-choose__img">
-              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/top_recruit_3.webp" alt="高校生" width="350" height="350">
+              <picture>
+                <source srcset="<?php echo get_template_directory_uri() ?>/images/common/top_recruit_3.webp" media="(min-width: 768px)" width="350" height="350" />
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/top_recruit_3_sp.jpg" alt="高校生" width="350" height="190">
+              </picture>
             </figure>
             <div class="p-top-choose__detail">
               <h4 class="p-top-choose__list-title">高校生</h4>
               <p class="p-top-choose__text">一人ひとりとじっくり向き合った進路指導を</p>
-              <a href="#" class="p-top-choose__btn">詳しくはこちら</a>
+              <div class="p-top-choose__btn-wrapper">
+                <a href="#" class="p-top-choose__btn">詳しくはこちら</a>
+              </div>
             </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+  <section class="p-top-room">
+    <div class="l-inner">
+      <div class="p-top-room__title">
+        <h3 class="c-main-title">教室を選ぶ</h3>
+      </div>
+      <div class="p-top-room__content">
+        <ul class="p-top-room__lists">
+          <li class="p-top-room__list">
+            <a href="#" class="p-top-room__link">
+              <figure class="p-top-room__img">
+                <picture>
+                  <source srcset="<?php echo get_template_directory_uri(); ?>/images/common/top_room_1.jpg" media="(min-width: 768px)" width="1238" height="570"/>
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/top_room_1_sp.jpg" alt="服部塾水口校" width="400" height="144">
+                </picture>
+              </figure>
+              <p class="p-top-room__text">服部塾水口校</p>
+            </a>
+          </li>
+          <li class="p-top-room__list">
+            <a href="#" class="p-top-room__link">
+              <figure class="p-top-room__img">
+                <picture>
+                  <source srcset="<?php echo get_template_directory_uri(); ?>/images/common/top_room_2.jpg" media="(min-width: 768px)" width="1238" height="570"/>
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/top_room_2_sp.jpg" alt="服部塾甲西駅前校" width="400" height="144">
+                </picture>
+              </figure>
+              <p class="p-top-room__text">服部塾甲西駅前校</p>
+            </a>
+          </li>
+          <li class="p-top-room__list">
+            <a href="#" class="p-top-room__link">
+              <figure class="p-top-room__img">
+                <picture>
+                  <source srcset="<?php echo get_template_directory_uri(); ?>/images/common/top_room_3.jpg" media="(min-width: 768px)" width="1238" height="570"/>
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/top_room_3_sp.jpg" alt="よしき塾日野校" width="400" height="144">
+                </picture>
+              </figure>
+              <p class="p-top-room__text">よしき塾日野校</p>
+            </a>
+          </li>
+          <li class="p-top-room__list">
+            <a href="#" class="p-top-room__link">
+              <figure class="p-top-room__img">
+                <picture>
+                  <source srcset="<?php echo get_template_directory_uri(); ?>/images/common/top_room_4.jpg" media="(min-width: 768px)" width="1238" height="570"/>
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/top_room_4_sp.jpg" alt="服部塾甲南校" width="400" height="144">
+                </picture>
+              </figure>
+              <p class="p-top-room__text">服部塾甲南校</p>
+            </a>
           </li>
         </ul>
       </div>
@@ -53,12 +124,38 @@
       <div class="p-top-recruit__content">
         <div class="p-top-recruit__text-wrapper">
           <p class="p-top-recruit__text">
-            服部塾・よしき塾では一緒に働いていただける方を<br>
+            服部塾・よしき塾では一緒に働いて<br class="u-mobile">いただける方を<br class="u-desktop">
             随時募集しております。
           </p>
+        </div>
+        <div class="p-top-recruit__btn-wrapper">
+          <a href="tel:0748634588" class="p-top-recruit__btn">
+            <div class="p-top-recruit__btn-row">
+              <svg xmlns="http://www.w3.org/2000/svg" width="38.013" height="40.299">
+                <defs>
+                  <clipPath id="a">
+                    <path d="M0 0h38.013v40.299H0z" />
+                  </clipPath>
+                </defs>
+                <g clip-path="url(#a)">
+                  <path d="M9.782 14.1a2.261 2.261 0 00-.164-2.979c-.778-.878-.347-1.307-.347-1.307l9.273-1.848 9.273-1.849s.562.231.181 1.34a2.258 2.258 0 00.992 2.815 29.135 29.135 0 006.869.7c1.605-.149 2.17-2.069 2.154-2.582S38.165 2.677 33 .951 21.644.2 17.174 1.094 6.609 3.458 2.5 7.031s-1.96 8.31-1.777 8.79 1.44 2.037 2.98 1.559a29.226 29.226 0 006.079-3.28" />
+                  <path d="M20.827 32.075a5.271 5.271 0 10-3.729-1.543 5.244 5.244 0 003.729 1.543" />
+                  <path d="M29.67 13.031a.9.9 0 00-.8-.484h-2.657a.6.6 0 00-.6.6v1.819a.6.6 0 01-.6.6h-8.37a.6.6 0 01-.6-.6v-1.817a.6.6 0 00-.6-.6h-2.664a.9.9 0 00-.8.484l-6.551 12.54a15.291 15.291 0 00-1.739 7.089v2.926a3 3 0 003 3h.751v1.71h3.057v-1.705h20.658v1.71h3.057v-1.71h.75a3 3 0 003-3V32.66a15.308 15.308 0 00-1.74-7.089zM12.588 26.802a8.239 8.239 0 118.239 8.238 8.241 8.241 0 01-8.239-8.238" />
+                </g>
+              </svg>
+              <p class="p-top-recruit__num">0748-63-4588</p>
+            </div>
+            <p class="p-top-recruit__btn-bottom">詳細につきましてはお電話にてお問合せください</p>
+          </a>
         </div>
       </div>
     </div>
   </section>
+  <figure class="p-top-future">
+    <picture>
+      <source srcset="<?php echo get_template_directory_uri(); ?>/images/common/top_future.png" media="(min-width: 768px)" width="1238" height="570" />
+      <img src="<?php echo get_template_directory_uri(); ?>/images/common/top_future_sp.png" alt="君の輝く未来はここから始まる" width="376" height="397">
+    </picture>
+  </figure>
 </main>
 <?php get_footer() ?>
