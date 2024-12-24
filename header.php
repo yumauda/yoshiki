29f384537c47c5bb32f82791fa3114533fd0a93b
yumauda <firststep-yumauda@gmail.com>
2024-12-24 15:26:12 +0900

    header周り・コース部分の共通箇所

--- header.php
+++ header.php
@@ -76,8 +76,93 @@
 </head>
 
 <body <?php body_class(); ?>>
-    <header class="l-header p-header">
-        <div class="l-inner">
-            header.php
+    <header class="p-header">
+        <div class="p-header__inner">
+            <div class="p-header__content">
+                <h1 class="p-header__logo">
+                    <a href="<?php echo esc_url(home_url('/')); ?>" class="p-header__home">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/common/header_logo.svg" alt="服部塾よしき塾" width="212" height="35">
+                    </a>
+                </h1>
+                <nav class="p-header__nav">
+                    <ul class="p-header__lists">
+                        <li class="p-header__list">
+                            <a href="<?php echo esc_url(home_url('/')); ?>" class="p-header__link">
+                                トップページ
+                            </a>
+                        </li>
+                        <li class="p-header__list">
+                            <a href="<?php echo esc_url(home_url('/feature')); ?>" class="p-header__link">
+                                服部塾・よしき塾の特長
+                            </a>
+                        </li>
+                        <li class="p-header__list">
+                            <a href="<?php echo esc_url(home_url('/course')); ?>" class="p-header__link">
+                                各コースの料金と詳細
+                            </a>
+                        </li>
+                        <li class="p-header__list">
+                            <a href="<?php echo esc_url(home_url('/news')); ?>" class="p-header__link">
+                                お知らせ
+                            </a>
+                        </li>
+                        <li class="p-header__list">
+                            <a href="<?php echo esc_url(home_url('/')); ?>" class="p-header__link">
+                                お問い合わせ
+                            </a>
+                        </li>
+                        <li class="p-header__list">
+                            <a href="<?php echo esc_url(home_url('/')); ?>" class="p-header__link--btn">
+                                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri(); ?>/images/common/header_btn.png" alt="教室を探す" width="270" height="80">
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <button class="p-header__drawer p-drawer-icon">
+                    <span class="p-drawer-icon__bars">
+                        <span class="p-drawer-icon__bar1"></span>
+                        <span class="p-drawer-icon__bar3"></span>
+                    </span>
+                </button>
+                <div class="p-header__drawer-content p-drawer-content">
+                    <div class="p-drawer-content__items">
+                        <ul class="p-drawer-content__lists">
+                            <li class="p-drawer-content__list">
+                                <a href="<?php echo esc_url(home_url('/')); ?>" class="p-drawer-content__link">トップ</a>
+                            </li>
+                            <li class="p-drawer-content__list">
+                                <a href="<?php echo esc_url(home_url('/about')); ?>" class="p-drawer-content__link">TRUXiAについて</a>
+                            </li>
+                            <li class="p-drawer-content__list">
+                                <a href="<?php echo esc_url(home_url('/business')); ?>" class="p-drawer-content__link">事業紹介</a>
+                            </li>
+                            <li class="p-drawer-content__list">
+                                <a href="<?php echo esc_url(home_url('/talent')); ?>" class="p-drawer-content__link">タレント紹介</a>
+                            </li>
+                            <li class="p-drawer-content__list">
+                                <a href="<?php echo esc_url(home_url('/news')); ?>" class="p-drawer-content__link">お知らせ</a>
+                            </li>
+                        </ul>
+                        <div class="p-drawer-content__sns">
+                            <p class="p-drawer-content__sns-text">FOLLOW US</p>
+                            <a href="https://www.instagram.com/truxia.management/" class="p-drawer-content__sns-link" target="_blank">
+                                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/instagram.svg" alt="インスタグラム" width="30" height="30">
+                            </a>
+                            <a href="https://x.com/truxia_mg" class="p-drawer-content__sns-link" target="_blank">
+                                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/x.svg" alt="x" width="30" height="30">
+                            </a>
+                        </div>
+                        <div class="p-drawer-content__contact-wrapper">
+                            <a href="<?php echo esc_url(home_url('/contact')); ?>" class="p-drawer-content__contact">
+                                <p class="p-drawer-content__contact-text">お問い合わせ</p>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="15.5" height="4.81">
+                                    <path d="M.75 4.06h14l-2.831-3" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" />
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
         </div>
     </header>
--- page-course.php
+++ page-course.php
@@ -136,6 +136,138 @@
             </div>
           </div>
         </div>
+        <div class="p-school">
+          <div class="p-school__title-wrapper">
+            <div class="p-school__icon">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri(); ?>/images/common/girl_2.svg" alt="" width="431" height="38">
+            </div>
+            <h3 class="p-school__title">
+              中学生
+            </h3>
+            <div class="p-school__icon">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri(); ?>/images/common/boy_2.svg" alt="" width="431" height="38">
+            </div>
+          </div>
+          <div class="p-school__detail">
+            <div class="p-detail">
+              <div class="p-detail__title-wrapper">
+                <p class="p-detail__title">中学生コースの詳細</p>
+              </div>
+              <div class="p-detail__slider p-detail__slider--secondary">
+                <!-- Slider main container -->
+                <div class="swiper swiper-detail">
+                  <!-- Additional required wrapper -->
+                  <div class="swiper-wrapper swiper-wrapper--secondary">
+                    <!-- Slides -->
+                    <div class="swiper-slide">
+                      <figure class="p-detail__img">
+                        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri(); ?>/images/common/course_swiper5.jpg" alt="小6数コース" width="250" height="175">
+                      </figure>
+                      <div class="p-detail__block">
+                        <p class="p-detail__title">集団指導コース</p>
+                        <p class="p-detail__text">各教科別に行う予習型の授業です。中学校の進度を確認しながら、ポイントをおさえて分かりやすく丁寧に指導します。学校の授業内容を十分理解することで、高校入試に向けた内申点（成績）の向上を目指します。</p>
+                      </div>
+                    </div>
+                    
+                    <div class="swiper-slide">
+                      <figure class="p-detail__img">
+                        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri(); ?>/images/common/course_swiper6.jpg" alt="小６県立中受験コース" width="250" height="175">
+                      </figure>
+                      <div class="p-detail__block">
+                        <p class="p-detail__title">無料のテスト対策</p>
+                        <p class="p-detail__text">定期テスト前の休日には、無料のテスト対策授業（集団授業）を受けていただくことができます。テスト対策授業ではテスト頻出の重要単元の復習や過去問の演習を行い、高得点を目指します。個別指導コースを受講の方も集団授業のテスト対策に無料でご参加いただけます。</p>
+                      </div>
+                    </div>
+                    <div class="swiper-slide">
+                      <figure class="p-detail__img">
+                        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri(); ?>/images/common/course_swiper7.jpg" alt="個別指導コース" width="250" height="175">
+                      </figure>
+                      <div class="p-detail__block">
+                        <p class="p-detail__title">個別指導コース</p>
+                        <p class="p-detail__text">中学校の進度に合わせた予習型の授業や、苦手教科を克服するための授業、高校入試に向けた過去問の演習授業など、さまざまなニーズにお応えします。集団授業との併用も可能なので、集団授業で学校の予習、個別指導で苦手分野の復習といった受講の仕方も可能です。（教科担当制）</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <!-- If we need navigation buttons -->
+                  <div class="swiper-button-prev"></div>
+                  <div class="swiper-button-next"></div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="p-school">
+          <div class="p-school__title-wrapper">
+            <div class="p-school__icon">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri(); ?>/images/common/girl_3.svg" alt="" width="431" height="38">
+            </div>
+            <h3 class="p-school__title">
+              高校生
+            </h3>
+            <div class="p-school__icon">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri(); ?>/images/common/boy_3.svg" alt="" width="431" height="38">
+            </div>
+          </div>
+          <div class="p-school__detail">
+            <div class="p-detail">
+              <div class="p-detail__title-wrapper">
+                <p class="p-detail__title">高校生コースの詳細</p>
+              </div>
+              <div class="p-detail__slider">
+                <!-- Slider main container -->
+                <div class="swiper swiper-detail">
+                  <!-- Additional required wrapper -->
+                  <div class="swiper-wrapper">
+                    <!-- Slides -->
+                    <div class="swiper-slide">
+                      <figure class="p-detail__img">
+                        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri(); ?>/images/common/course_swiper8.jpg" alt="小6数コース" width="250" height="175">
+                      </figure>
+                      <div class="p-detail__block">
+                        <p class="p-detail__title">集団指導コース（高１・高２）</p>
+                        <p class="p-detail__text">高校で習う内容をより深く理解することはもちろん、大学入試を念頭に数学と英語の授業を行います。数学では定期テストだけでなく大学入試頻出の重要単元の演習を、英語は高２から共通テストや大学入試の過去問の演習にも取り組むことで、高３生になる前から受験に向けたアドバンテージをつくります。<br>また、定期テスト前には無料でテスト対策授業も行います。</p>
+                      </div>
+                    </div>
+                    <div class="swiper-slide">
+                      <figure class="p-detail__img">
+                        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri(); ?>/images/common/course_swiper9.jpg" alt="小6英語コース" width="250" height="175">
+                      </figure>
+                      <div class="p-detail__block">
+                        <p class="p-detail__title">集団指導コース（高３・高卒）</p>
+                        <p class="p-detail__text">大学入試共通テストや私立大学入試対策の授業を行います。大学入試に精通した精鋭の講師による、最新の入試問題の傾向をふまえた授業で志望校合格を目指します。映像授業ではなくライブ授業なので映像を見ながら寝てしまったり、分からないところが質問できなかったり、といったことはありません。</p>
+                      </div>
+                    </div>
+                    <div class="swiper-slide">
+                      <figure class="p-detail__img">
+                        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri(); ?>/images/common/course_swiper10.jpg" alt="小６県立中受験コース" width="250" height="175">
+                      </figure>
+                      <div class="p-detail__block">
+                        <p class="p-detail__title">個別指導コース</p>
+                        <p class="p-detail__text">高校の進度に合わせた授業や、大学入試に受けた過去問の演習など、さまざまなニーズにお応えします。授業では問題の解説だけでなく、定期テストや大学入試に向けた日頃の勉強法などのアドバイスもさせていただきます。</p>
+                      </div>
+                    </div>
+                    <div class="swiper-slide">
+                      <figure class="p-detail__img">
+                        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri(); ?>/images/common/course_swiper11.jpg" alt="個別指導コース" width="250" height="175">
+                      </figure>
+                      <div class="p-detail__block">
+                        <p class="p-detail__title">土日も使える自習室</p>
+                        <p class="p-detail__text">水口校・甲西駅前校は土日も自習室が使えます。「家では集中できない」「一人だとなかなか自分から勉強できない」といった生徒さんからも好評です。主な大学の赤本も揃っており、コピー機も使えるなど勉強できる環境が整っています。</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <!-- If we need navigation buttons -->
+                  <div class="swiper-button-prev"></div>
+                  <div class="swiper-button-next"></div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
