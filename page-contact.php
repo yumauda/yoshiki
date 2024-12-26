839a56a4f4dbb908a305ff130e68d805b0ac08d1
yumauda <firststep-yumauda@gmail.com>
2024-12-26 11:01:49 +0900

    お問い合わせページサンクスページSP化

--- page-contact.php
+++ page-contact.php
@@ -10,8 +10,8 @@
           <h2 class="c-main-title">お問い合わせ</h2>
         </div>
         <div class="p-contact__attention">
-          <p class="p-contact__attention-text">無料体験をお申し込みの方は、お問い合わせフォームの<br>
-            お問い合わせ項目より「無料体験申し込み」をお選びください。</p>
+          <p class="p-contact__attention-text">無料体験をお申し込みの方は、<br class="u-mobile">お問い合わせフォームの<br class="u-desktop">
+            お問い合わせ項目より<br class="u-mobile">「無料体験申し込み」をお選びください。</p>
         </div>
         <?php the_content(); ?>
         <div class="p-form">
--- page-thanks.php
+++ page-thanks.php
@@ -12,12 +12,12 @@
         <div class="p-thanks__block">
           <div class="p-thanks__text-wrapper">
             <p class="p-thanks__text">お問い合わせありがとうございます。<br>
-              お送り頂きました内容を確認の上、一週間以内に折り返しご連絡させて頂きます。<br>
-              また、ご記入頂いたメールアドレスへ、自動返信の確認メールをお送りしております。</p>
-            <p class="p-thanks__text">しばらく経ってもメールが届かない場合は、入力頂いたメールアドレスが間違っているか、<br>
-              迷惑メールフォルダに振り分けられている可能性がございます。<br>
+              お送り頂きました内容を確認の上、<br class="u-mobile">一週間以内に折り返しご連絡させて頂きます。<br>
+              また、ご記入頂いたメールアドレスへ、<br class="u-mobile">自動返信の確認メールをお送りしております。</p>
+            <p class="p-thanks__text">しばらく経ってもメールが届かない場合は、<br class="u-mobile">入力頂いたメールアドレスが間違っているか、<br>
+              迷惑メールフォルダに振り分けられている<br class="u-mobile">可能性がございます。<br>
               また、ドメイン指定をされている場合は、<br>
-              「@XXXX.com」からのメールが受信できるようあらかじめ設定をお願いいたします。</p>
+              「@XXXX.com」からのメールが受信できるよう<br class="u-mobile">あらかじめ設定をお願いいたします。</p>
           </div>
           <div class="p-thanks__btn-wrapper">
             <a href="<?php echo home_url(); ?>" class="p-thanks__btn">
