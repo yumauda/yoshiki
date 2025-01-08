50e4912d84fcf3eda8c8b82e15faef32d5c76ac4
yumauda <firststep-yumauda@gmail.com>
2025-01-08 15:47:04 +0900

    totopボタンホバー時の挙動

--- includes/totop.php
+++ includes/totop.php
@@ -1,5 +1,10 @@
 <div class="p-totop">
     <a href="#" class="p-totop__link">
-        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri(); ?>/images/common/back_top.svg" alt="トップへ戻る" width="100" height="100">
+        <span class="p-totop__arrow">
+            <svg xmlns="http://www.w3.org/2000/svg" width="13.033" height="12.385" viewBox="0 0 13.033 12.385">
+                <path d="M5.868,0,4.836,1.032,9.591,5.787H0V7.246H9.591L4.836,12l1.032,1.032,6.517-6.517Z" transform="translate(0 12.385) rotate(-90)" />
+            </svg>
+        </span>
+        <p class="p-totop__text">TOP</p>
     </a>
 </div>
